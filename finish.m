ebdfaf1580292f2b2ed6c23478a91aa847008ebc
srinivas.gs <github@srinivas.gs>
2016-12-02 15:27:09 -0500

    updated MATLAB startup and finish scripts

--- finish.m
+++ finish.m
@@ -4,3 +4,9 @@
 
 % check if there are uncommitted changes to any of the project repositories 
 isCodeCommitted;
+
+% unmount USB drive
+if ismac
+	disp('Attempting to unmount USB drive...')
+	system('diskutil unmount /Volumes/sgs_data')
+end
--- startup.m
+++ startup.m
@@ -17,41 +17,41 @@ if isempty(strfind(path1,':/usr/local/bin'))
 end
 setenv('PATH', path1);
 
-disp('Checking to see if Emonetlab backup is online...')
-if exist('/Volumes/home/srinivas_gs/','dir') == 7
-	disp('Emonetlab backup is online.')
-else
-	disp('Emonetlab backup is not online.')
-	% check if you're on the Yale network
-	[~,wifi] = unix('airport -I');
-	[~,a]=unix('ps aux | grep "Cisco" | wc -l');
-	if ~isempty(strfind(wifi,'Yale')) || str2double(a) > 2
-		disp('On Yale network, will try to mount Emonetlab backup...')
-		unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
-		pause(2)
-		% check if its online
-		if exist('/Volumes/home/srinivas_gs/','dir') == 7
-			disp('Emonetlab backup successfully mounted.')
-		else
-			% maybe took too long?
+% disp('Checking to see if Emonetlab backup is online...')
+% if exist('/Volumes/home/srinivas_gs/','dir') == 7
+% 	disp('Emonetlab backup is online.')
+% else
+% 	disp('Emonetlab backup is not online.')
+% 	% check if you're on the Yale network
+% 	[~,wifi] = unix('airport -I');
+% 	[~,a]=unix('ps aux | grep "Cisco" | wc -l');
+% 	if ~isempty(strfind(wifi,'Yale')) || str2double(a) > 2
+% 		disp('On Yale network, will try to mount Emonetlab backup...')
+% 		unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
+% 		pause(2)
+% 		% check if its online
+% 		if exist('/Volumes/home/srinivas_gs/','dir') == 7
+% 			disp('Emonetlab backup successfully mounted.')
+% 		else
+% 			% maybe took too long?
 			
-		end
-	else
-		disp('Not connected to Yale internet...')
-		if ~isempty(strfind(getComputerName,'yale'))
-			disp('On Yale VPN, will try to mount Emonetlab backup...')
-			unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
-			pause(2)
-			% check if its online
-			if exist('/Volumes/home/srinivas_gs/','dir') == 7
-				disp('Emonetlab backup successfully mounted.')
-			end
+% 		end
+% 	else
+% 		disp('Not connected to Yale internet...')
+% 		if ~isempty(strfind(getComputerName,'yale'))
+% 			disp('On Yale VPN, will try to mount Emonetlab backup...')
+% 			unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
+% 			pause(2)
+% 			% check if its online
+% 			if exist('/Volumes/home/srinivas_gs/','dir') == 7
+% 				disp('Emonetlab backup successfully mounted.')
+% 			end
 		
-		else
-			warning('Cant mount Emonetlab backup.')
-		end
-	end
-end
+% 		else
+% 			warning('Cant mount Emonetlab backup.')
+% 		end
+% 	end
+% end
 
 
 % Configuring POI library for Excel export...
@@ -69,5 +69,12 @@ set(0, 'DefaultFigurePosition', [-1790 200 1300 700]);
 
 % check if all code is committed
 isCodeCommitted
+
+% check if the USB drive is connected
+if exist('/Volumes/sgs_data','dir') == 7
+	disp('USB drive connected')
+else
+	cprintf('red','Connect USB drive!')
+end
 
 disp('All DONE!')
