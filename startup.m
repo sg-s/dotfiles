35985b702291fca5d5ec640725782f6df6f09975
srinivas.gs <github@srinivas.gs>
2016-12-16 14:47:09 -0500

    added python paths for matlab

--- startup.m
+++ startup.m
@@ -17,41 +17,14 @@ if isempty(strfind(path1,':/usr/local/bin'))
 end
 setenv('PATH', path1);
 
-% disp('Checking to see if Emonetlab backup is online...')
-% if exist('/Volumes/home/srinivas_gs/','dir') == 7
-% 	disp('Emonetlab backup is online.')
-% else
-% 	disp('Emonetlab backup is not online.')
-% 	% check if you're on the Yale network
-% 	[~,wifi] = unix('airport -I');
-% 	[~,a]=unix('ps aux | grep "Cisco" | wc -l');
-% 	if ~isempty(strfind(wifi,'Yale')) || str2double(a) > 2
-% 		disp('On Yale network, will try to mount Emonetlab backup...')
-% 		unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
-% 		pause(2)
-% 		% check if its online
-% 		if exist('/Volumes/home/srinivas_gs/','dir') == 7
-% 			disp('Emonetlab backup successfully mounted.')
-% 		else
-% 			% maybe took too long?
-			
-% 		end
-% 	else
-% 		disp('Not connected to Yale internet...')
-% 		if ~isempty(strfind(getComputerName,'yale'))
-% 			disp('On Yale VPN, will try to mount Emonetlab backup...')
-% 			unix('open -n /code/MATLAB/mount_emonetlab_backup.app --args -AppCommandLineArg');
-% 			pause(2)
-% 			% check if its online
-% 			if exist('/Volumes/home/srinivas_gs/','dir') == 7
-% 				disp('Emonetlab backup successfully mounted.')
-% 			end
-		
-% 		else
-% 			warning('Cant mount Emonetlab backup.')
-% 		end
-% 	end
-% end
+% paths for Multicore TSNE
+[~,home_path] = system('cd ~; pwd');
+a_path = strrep('~/anaconda3/bin','~',strtrim(home_path));
+path1 = getenv('PATH');
+if isempty(strfind(path1,[pathsep a_path]))
+    path1 = [a_path pathsep path1];
+end
+setenv('PATH', path1);
 
 
 % Configuring POI library for Excel export...
